d4af1e2a19494f177c152de76441c48045724daf
Randy Lai <randy.cs.lai@gmail.com>
2019-10-31 23:05:01 -0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# GitHub Action running rchk for an R package
+
+Just put this in your workflow
+
+```yml
+  rchk:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v1
+    - uses: randy3k/github-action-rchk
+      with:
+        apt: <any apt-get dependencies>
+        package: <your R package name>
+```
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eo pipefail
+
+cd /opt/R-svn/
+. /opt/rchk/scripts/cmpconfig.inc
+cd -
+
+APT="$1"
+if [ -n "$APT" ]; then
+    sudo apt-get install $APT -y
+fi
+
+R --slave -e "install.packages('remotes', repos = 'https://cloud.r-project.org')"
+
+R --slave -e "remotes::install_local(repos = 'https://cloud.r-project.org', upgrade = 'never')"
+
+PACKAGE="$2"
+
+echo "PACKAGE = $PACKAGE"
+
+cd /opt/R-svn/
+. /opt/rchk/scripts/cmpconfig.inc
+/opt/rchk/scripts/check_package.sh $PACKAGE
+if [ $(cat packages/lib/$PACKAGE/libs/$PACKAGE.so.bcheck | wc -l) -gt 1 ]; then
+  cat packages/lib/$PACKAGE/libs/$PACKAGE.so.bcheck
+  FAIL=1
+fi
+if [ $(cat packages/lib/$PACKAGE/libs/$PACKAGE.so.maacheck | wc -l) -gt 1 ]; then
+  cat packages/lib/$PACKAGE/libs/$PACKAGE.so.maacheck
+  FAIL=1
+fi
+[ -z "$FAIL" ] || exit 1
